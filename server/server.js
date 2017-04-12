1354e403ed3ba4e62313d84ba0c2c467d5202d31
Patrick <phubbard89@gmail.com>
2017-04-12 00:03:59 +0000

    created file tree

---
+++ server/server.js
@@ -0,0 +1,15 @@
+/*express resources*/
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var http = require('http');
+var Users = require('./models/...');
+var Movie = require('./models/...');
+
+/*api resources*/
+var unirest = require('unirest');
+var events = require('events');
+
+/*db resources*/
+var mongoose = require('mongoose');
+var config = require('./server/config');
