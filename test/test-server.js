9fef5a1b3232cb4ddfdea7c3698daf06379bcbbe
Patrick <phubbard89@gmail.com>
2017-04-27 00:59:51 +0000

    fixed delete test

--- test/test-server.js
+++ test/test-server.js
@@ -6,71 +6,64 @@ var chaiHttp = require('chai-http');
 var should = chai.should();
 
 var server = require('../server');
+var runServer = require("../server")
 var bookItem = require('../models/book');
-var {
-    runServer,
-    closeServer
-} = require("../server")
 var app = server.app;
 
 chai.use(chaiHttp);
 
 describe('Book Search', function() {
-            before(function() {
-                return runServer();
+
+    it('should list items on GET', function(done) {
+        chai.request(app)
+            .get('/book/:searchTerm')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.items.should.have.length(12);
             });
+        done();
+    });
 
-            after(function() {
-                return closeServer();
+    it('should list items on GET', function(done) {
+        chai.request(app)
+            .get('/populate-cart')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
             });
+        done();
+    });
 
-            it('should list items on GET', function(done) {
-                chai.request(app)
-                    .get('/book/:searchTerm')
-                    .end(function(err, res) {
-                        res.should.have.status(200);
-                        res.should.be.json;
-                        res.body.should.be.a('object');
-                        res.body.items.should.have.length(12);
-                    });
-                done();
+
+    it('should add an item on POST', function(done) {
+        chai.request(app)
+            .post('/add-to-cart')
+            .send({
+                'idValue': 'wcgw',
+                'link': 'https://en.wikipedia.org/wiki/Dune_(novel)',
+                'name': 'Dune'
+            })
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('idValue');
+                res.body.should.have.property('link');
+                res.body.should.have.property('name');
+                res.body.idValue.should.be.a('string');
+                res.body.link.should.be.a('string');
+                res.body.name.should.be.a('string');
             });
+        done();
+    });
 
-            it('should list items on GET', function(done) {
-                chai.request(app)
-                    .get('/populate-cart')
-                    .end(function(err, res) {
-                        res.should.have.status(200);
-                        res.should.be.json;
-                    });
-                done();
-            });
-
-
-            it('should add an item on POST', function(done) {
-                chai.request(app)
-                    .post('/add-to-cart')
-                    .send({
-                        'idValue': 'wcgw',
-                        'link': 'https://en.wikipedia.org/wiki/Dune_(novel)',
-                        'name': 'Dune'
-                    })
-                    .end(function(err, res) {
-                        should.equal(err, null);
-                        res.should.have.status(201);
-                        res.should.be.json;
-                        res.body.should.be.a('object');
-                        res.body.should.have.property('idValue');
-                        res.body.should.have.property('link');
-                        res.body.should.have.property('name');
-                        res.body.idValue.should.be.a('string');
-                        res.body.link.should.be.a('string');
-                        res.body.name.should.be.a('string');
-                    });
-                done();
-            });
-
-            it('should remove an item on DELETE', function(done) {
+    it('should remove an item on DELETE', function(done) {
+        chai.request(app)
+            .get('/add-to-cart')
+            .end(function(err, res) {
                 chai.request(app)
                     .delete('/delete-cart')
                     .end(function(err, res) {
@@ -82,6 +75,7 @@ describe('Book Search', function() {
                         res.body.should.have.property('link');
                         res.body.should.have.property('name');
                     });
-                done();
             });
+        done();
+    });
 });
